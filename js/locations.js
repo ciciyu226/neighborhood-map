bace14e492f1b11588c494f77ceb1571657b04c1
Cici Yu <ciciyu226@gmail.com>
2017-03-23 16:05:58 -0700

    Setting up project file

---
+++ js/locations.js
@@ -0,0 +1,16 @@
+/* Author: Cici Yu
+ * Date: 3.23.2017
+ * Description: This is a local database model that stores location infos.
+ */
+
+var locations = [{
+
+    },{
+
+    },{
+
+    },{
+
+    },{
+
+    }]
